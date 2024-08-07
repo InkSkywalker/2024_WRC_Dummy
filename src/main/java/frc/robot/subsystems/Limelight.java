0d838c3cb5dffb5c3b687b306dc88830b597f769
MapleWithered <wangsj2000@126.com>
2024-08-08 03:56:00 +0800

    Limelight working in AdvantageScope

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LimelightHelpers;
 import frc.robot.generated.TunerConstants;
 
-public class Limelight extends SubsystemBase  {
+public class Limelight extends SubsystemBase {
 
     Swerve m_swerve = TunerConstants.DriveTrain;
     Pigeon2 m_gyro = new Pigeon2(20, "canivore");
@@ -29,28 +29,33 @@ public class Limelight extends SubsystemBase  {
         boolean doRejectUpdate = false;
         LimelightHelpers.SetRobotOrientation("limelight", m_swerve.getState().Pose.getRotation().getDegrees(), 0, 0, 0,
                 0, 0);
-        LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+        LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight");
         if (Math.abs(m_gyro.getRate()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore
                                               // vision updates
         {
             doRejectUpdate = true;
         }
-        if (mt2.tagCount == 0) {
+        if (mt2.tagCount <= 1) {
             doRejectUpdate = true;
         }
         if (!doRejectUpdate) {
-            m_swerve.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, 9999999));
+            var stddev = VecBuilder.fill(.7, .7, 5);
+            if (mt2.tagCount == 1) {
+                stddev = VecBuilder.fill(50, 50, 999);
+            }
+            if (mt2.tagCount == 2) {
+                stddev = VecBuilder.fill(1, 1, 10);
+            }
+            m_swerve.setVisionMeasurementStdDevs(stddev);
             m_swerve.addVisionMeasurement(
                     mt2.pose,
                     mt2.timestampSeconds);
+            limelightPub.set(new double[] {
+                    mt2.pose.getX(),
+                    mt2.pose.getY(),
+                    mt2.pose.getRotation().getDegrees()
+            });
         }
-
-        limelightPub.set(new double[] {
-                mt2.pose.getX(),
-                mt2.pose.getY(),
-                mt2.pose.getRotation().getDegrees()
-        });
-
     }
 
     public static Pose2d getLimelightPose() {
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -5,9 +5,12 @@ import java.util.function.Supplier;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.SteerRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -15,6 +18,7 @@ import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.generated.TunerConstants;
 
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements
@@ -25,6 +29,22 @@ public class Swerve extends SwerveDrivetrain implements Subsystem {
     private Notifier m_simNotifier = null;
     private double m_lastSimTime;
 
+    public boolean take_control_yaw = false;
+    public double yaw_setpoint = 0.;
+    public double yaw_controller_output = 0;
+    public PIDController m_yawController = new PIDController(0.1, 0, 0);
+
+    public static double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
+    public static double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+
+    public static final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.Velocity) // I want field-centric
+                                                             // driving in open loop
+            .withSteerRequestType(SteerRequestType.MotionMagic);
+    public static final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    public static final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
     /* Blue alliance sees forward as 0 degrees (toward red alliance wall) */
     private final Rotation2d BlueAlliancePerspectiveRotation = Rotation2d.fromDegrees(0);
     /* Red alliance sees forward as 180 degrees (toward blue alliance wall) */
@@ -32,12 +52,14 @@ public class Swerve extends SwerveDrivetrain implements Subsystem {
     /* Keep track if we've ever applied the operator perspective before or not */
     private boolean hasAppliedOperatorPerspective = false;
 
-    public Swerve(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
+    public Swerve(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency,
+            SwerveModuleConstants... modules) {
         super(driveTrainConstants, OdometryUpdateFrequency, modules);
         if (Utils.isSimulation()) {
             startSimThread();
         }
     }
+
     public Swerve(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
         super(driveTrainConstants, modules);
         if (Utils.isSimulation()) {
@@ -47,6 +69,26 @@ public class Swerve extends SwerveDrivetrain implements Subsystem {
 
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
         return run(() -> this.setControl(requestSupplier.get()));
+    }
+
+    public Command drive_cmd(double x, double y, double rot) {
+        // double rot_output;
+        // if (take_control_yaw) {
+        //     rot_output = yaw_controller_output;
+        // }
+        // else {
+        //     rot_output = rot;
+        // }
+        System.out.println(x + " " + y + " " + rot);
+        return this.applyRequest(() -> drive.withVelocityX(x).withVelocityY(y).withRotationalRate(rot));
+    }
+
+    public Command brake_cmd() {
+        return applyRequest(() -> brake);
+    }
+
+    public Command pointWheelsAt_cmd(double x, double y) {
+        return applyRequest(() -> point.withModuleDirection(new Rotation2d(x, y)));
     }
 
     private void startSimThread() {
@@ -67,10 +109,22 @@ public class Swerve extends SwerveDrivetrain implements Subsystem {
     @Override
     public void periodic() {
         /* Periodically try to apply the operator perspective */
-        /* If we haven't applied the operator perspective before, then we should apply it regardless of DS state */
-        /* This allows us to correct the perspective in case the robot code restarts mid-match */
-        /* Otherwise, only check and apply the operator perspective if the DS is disabled */
-        /* This ensures driving behavior doesn't change until an explicit disable event occurs during testing*/
+        /*
+         * If we haven't applied the operator perspective before, then we should apply
+         * it regardless of DS state
+         */
+        /*
+         * This allows us to correct the perspective in case the robot code restarts
+         * mid-match
+         */
+        /*
+         * Otherwise, only check and apply the operator perspective if the DS is
+         * disabled
+         */
+        /*
+         * This ensures driving behavior doesn't change until an explicit disable event
+         * occurs during testing
+         */
         if (!hasAppliedOperatorPerspective || DriverStation.isDisabled()) {
             DriverStation.getAlliance().ifPresent((allianceColor) -> {
                 this.setOperatorPerspectiveForward(
@@ -79,5 +133,8 @@ public class Swerve extends SwerveDrivetrain implements Subsystem {
                 hasAppliedOperatorPerspective = true;
             });
         }
+
+        this.yaw_controller_output = m_yawController.calculate(this.getState().Pose.getRotation().getDegrees(), this.yaw_setpoint);
+        
     }
 }
